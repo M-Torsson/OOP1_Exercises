a7fcef0a6072b9db6155de785755e9904c971e1f
Muthana Torsson <muthana.tursson@gmail.com>
2026-03-18 12:25:15 +0200

    Fix: added actual code

--- src/main/java/Lexcon/BankAccount.java
+++ src/main/java/Lexcon/BankAccount.java
@@ -1,4 +1,28 @@
 package Lexcon;
 
 public class BankAccount {
+
+    private String accountHolder;
+    private double balance;
+
+    public BankAccount(String accountHolder, double balance) {
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public void deposit(double amount) {
+        balance = balance + amount;
+    }
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance = balance - amount;
+        } else {
+            System.out.println("Not enough money");
+        }
+    }
 }
--- src/main/java/Lexcon/Customer.java
+++ src/main/java/Lexcon/Customer.java
@@ -1,4 +1,17 @@
 package Lexcon;
 
 public class Customer {
+
+    private String name;
+    private BankAccount account;
+
+    public Customer(String name, BankAccount account) {
+        this.name = name;
+        this.account = account;
+    }
+
+    public BankAccount getAccount() {
+        return account;
+    }
+
 }
