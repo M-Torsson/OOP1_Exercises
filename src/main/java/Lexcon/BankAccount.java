de99eb48870e60129bbb33dbd549821888fa61b1
Muthana Torsson <muthana.tursson@gmail.com>
2026-03-18 12:36:22 +0200

    Fix: added actual code

--- src/main/java/Lexcon/BankAccount.java
+++ src/main/java/Lexcon/BankAccount.java
@@ -9,15 +9,19 @@ public class BankAccount {
         this.accountHolder = accountHolder;
         this.balance = balance;
     }
+
     public String getAccountHolder() {
         return accountHolder;
     }
+
     public double getBalance() {
         return balance;
     }
+
     public void deposit(double amount) {
         balance = balance + amount;
     }
+
     public void withdraw(double amount) {
         if (amount <= balance) {
             balance = balance - amount;
